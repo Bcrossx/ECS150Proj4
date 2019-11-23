caf33db12b409c93f75f783e00166a8b80ee1809
bfcrossl <bfcrossl@ad3.ucdavis.edu@pc44.cs.ucdavis.edu>
2019-11-22 18:13:06 -0800

    Phase 1 complete. Pretty much

--- libfs/fs.c
+++ libfs/fs.c
@@ -12,15 +12,17 @@
  // blocks containing the meta-information
  // about the file system (superblock, FAT and root directory)
 
+//struct fileDescriptors
+
 //file system structs (superblock,FAT)
 struct __attribute__((__packed__)) superBlock
 {
   unint8_t signature [8];
-  unint2_t totalblockCount;
-  unint2_t rootDir_blockIndex;
-  unint2_t datablock_startIndex;
-  unint2_t datablockCount;
-  unint2_t fatblockCount;
+  unint16_t totalblockCount;
+  unint16_t rootDir_blockIndex;
+  unint16_t datablock_startIndex;
+  unint16_t datablockCount;
+  unint8_t fatblockCount;
   unint8_t padding[4079];
 };
 
@@ -41,25 +43,92 @@ struct __attribute__((__packed__)) fsMeta
 
 static struct fsMeta* currFS;
 
+bool mountedDisk;
+
 int fs_mount(const char *diskname)
 {
-	/* TODO: Phase 1 */
   // Open the virtual disk
   // Read the metadata (superblock, fat, root directory)
+
+  //Open virtual disk
+  int diskOpen = NULL;
+  diskOpen = block_disk_open(diskname);
+
+  //read in superblock on index 0
+  block_read(0, currFS.superblock);
+
+  //malloc fat
+  currFS.fat = malloc(superblock.fatblockCount * BLOCK_SIZE);
+
+  for(int fat = 1; fat <= superblock.fatblockCount; fat++){ //iterate through fat entries
+
+      //First argument is the index, fat starts at index 1
+      //Second argument is the amount we want to read
+      block_read(fat, currFS.fat+(BLOCK_SIZE/2)*(fat-1));
+
+  }
+
+  //read in root directory
+  block_read(rootDir_blockIndex, currFS.rootDir);
+
+
+  if(diskOpen == -1)
+    return -1;
+  if(diskOpen != NULL) //if disk open set boolean variable to true
+    mountedDisk = TRUE;
+
+  return 0;
 }
 
 int fs_umount(void)
 {
-	/* TODO: Phase 1 */
   //Close virtual disk (make sure that virtual disk is up-to-date)
+
+  //for this function we need to update any changes made with the Write
+  //function. We start with the fat because the superblock cannot be changed.
+  for(int fat = 1; fat <= superblock.fatblockCount; fat++){ //iterate through fat entries
+
+      block_write(fat, currFS.fat+(BLOCK_SIZE/2)*(fat-1));
+
+  }
+
+  //write root directory
+  block_write(rootDir_blockIndex, currFS.rootDir);
+
+  //close disk
+  block_disk_close();
+
+  if(block_disk_close() != -1)
+    mountedDisk = FALSE;
+  if(block_disk_close() == -1)
+    return -1;
+
+  return 0;
 
 }
 
 int fs_info(void)
 {
-	/* TODO: Phase 1 */
   //Show information about volume
 
+//   FS Info:
+// total_blk_count=4100
+// fat_blk_count=2
+// rdir_blk=3
+// data_blk=4
+// data_blk_count=4096
+// fat_free_ratio=4095/4096
+// rdir_free_ratio=128/128
+  if(mountedDisk = FALSE) // Could cause an issue
+    return -1;
+  printf("\t FS Info: \n");
+  printf("total_blk_count='%d'", totalblockCount);
+  printf("fat_blk_count='%d'", fatblockCount);
+  printf("rdir_blk='%d'", rootDir);
+  printf("data_blk='%d'", datablock_startIndex);
+  printf("data_blk_count='%d'", datablockCount);
+  printf("fat_free_ratio='%d'", BLOCK_SIZE-fatblockCount "/" BLOCK_SIZE);
+  printf("rdir_free_ratio='%d'",  "/" );
 }
 
 int fs_create(const char *filename)
@@ -91,7 +160,6 @@ int fs_open(const char *filename)
 // Can open same file multiple times
 // Contains file's offset (initially 0)
 
-//fd = open(*filename, flags);
 
 }
 
